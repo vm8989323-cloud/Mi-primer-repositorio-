c1497e17ded44a59f5058474a066802ebe40a519
vm8989323-cloud <vm8989323@gmail.com>
2026-05-05 13:16:36 -0500

    Create Variables.py forma variable hand

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#Inicializando variables y colocandoles un valor
+nombre= " valería morales Bustamante"
+edad= 14
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
